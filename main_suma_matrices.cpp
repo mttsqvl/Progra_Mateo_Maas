55c50f1cbd6f8aeb8e2f26d6954efe14d34b6b51
mttsqvl <136513336+mttsqvl@users.noreply.github.com>
2023-06-18 23:44:42 -0600

    Update main_suma_matrices.cpp

--- main_suma_matrices.cpp
+++ main_suma_matrices.cpp
@@ -3,37 +3,30 @@
 using namespace std;
 int fila=4;
 int columna=4;
-int i=0;
-int b=0;
-int c=0;
-int d=0;
-int e=0;
-int f=0;
 int sumafila[4];
 int sumacolumna[4];
 int main() {
 	int A[fila][columna];
-	cout<<"Ingresa los números que quieres que tenga la matriz."<<endl;
-	for(i; i<fila; i++){
-	for(b; b<columna; b++){	
-	cout<<"Posición: "<<i<<" "<<b<<endl;
+	cout<<"Ingresa los nÃºmeros que quieres que tenga la matriz."<<endl;
+	for(int i=0; i<fila; i++){
+	for(int b=0; b<columna; b++){	
+	cout<<"PosiciÃ³n: "<<i<<" "<<b<<endl;
 	cin>>A[i][b];	
 }
 }
 	cout<<"La matriz es:"<<endl;
 	
-	for(c; c<fila; c++){
-	for(d; d<columna; d++){
-   cout<<A[c][d]<<"   "<<A[c][d]<<"   "<<A[c][d]<<"   "<<A[c][d]<<endl;
+	for(int c=0; c<fila; c++){
+
+   cout<<A[c][0]<<"   "<<A[c][1]<<"   "<<A[c][2]<<"   "<<A[c][3]<<endl;
 }
-}
-	for(e; e<fila; e++){
+	for(int e=0; e<fila; e++){
 	sumafila[e]=A[e][0]+A[e][1]+A[e][2]+A[e][3];
 		
 	cout<<"La suma correspondiente de las fila "<<e<<" es de: "<<sumafila[e]<<endl;
 	
 }
-	for(f; f<columna; f++){
+	for(int f=0; f<columna; f++){
 	sumacolumna[f]=A[0][f]+A[1][f]+A[2][f]+A[3][f];
 		
 	cout<<"La suma correspondiente de las columna "<<f<<" es de: "<<sumacolumna[f]<<endl;
