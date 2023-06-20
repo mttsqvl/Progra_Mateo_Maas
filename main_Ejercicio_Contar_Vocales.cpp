61ff7bae2efff87e976940fd49ab727fa93fae18
mttsqvl <136513336+mttsqvl@users.noreply.github.com>
2023-06-20 15:04:01 -0600

    Add files via upload

---
+++ main_Ejercicio_Contar_Vocales.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	int x=5;
+	char A[x];
+
+	cout<<"Ingresa una frase."<<endl;
+	cin.getline(A, x, '/n');
+	
+	
+	
+	int Y = strlen(A); 
+	if(A=="a",A=="e",A=="i",A=="o",A=="u"){
+	int Y = strlen(A);
+	cout<<"El número de vocales es de:"<<Y<<endl;
+	}	
+	
+	}
+
+
